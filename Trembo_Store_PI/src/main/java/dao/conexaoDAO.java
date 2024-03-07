c4ec0b07acc54d298f4c627594f7485557827737
Gustavo Enrico <gustavoenrico238@gmail.com>
2024-03-07 19:38:18 -0300

    Origanizando arquivos

--- Trembo_Store_PI/src/main/java/dao/conexaoDAO.java
+++ Trembo_Store_PI/src/main/java/dao/conexaoDAO.java
@@ -1,23 +1,23 @@
-package dao;
-
-import javax.swing.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class conexaoDAO {
-
-    public Connection conectaBD() {
-        Connection conexao = null;
-
-        try {
-            String url = "jdbc:mysql://localhost:3306/trembo_store?user=root&password=3045";
-            conexao = DriverManager.getConnection(url);
-
-        } catch (SQLException erro) {
-            JOptionPane.showMessageDialog(null, "Erro na classe de conexão do BD" + erro.getMessage());
-        }
-        return conexao;
-    }
-
-}
+//package dao;
+//
+//import javax.swing.*;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class conexaoDAO {
+//
+//    public Connection conectaBD() {
+//        Connection conexao = null;
+//
+//        try {
+//            String url = "jdbc:mysql://localhost:3306/trembo_store?user=root&password=3045";
+//            conexao = DriverManager.getConnection(url);
+//
+//        } catch (SQLException erro) {
+//            JOptionPane.showMessageDialog(null, "Erro na classe de conexão do BD" + erro.getMessage());
+//        }
+//        return conexao;
+//    }
+//
+//}
