4c189424459e770a490004c107cfcf1fb1e4d0f3
Altieres <altieres_lima_capim@hotmail.com>
2024-03-07 19:29:04 -0300

    Adicionando Spring boot

---
+++ src/main/java/dao/conexaoDAO.java
@@ -0,0 +1,23 @@
+//package dao;
+//
+//import javax.swing.*;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class conexaoDAO {
+//
+//    public Connection conectaBD() {
+//        Connection conexao = null;
+//
+//        try {
+//            String url = "jdbc:mysql://localhost:3306/trembo_store?user=root&password=3045";
+//            conexao = DriverManager.getConnection(url);
+//
+//        } catch (SQLException erro) {
+//            JOptionPane.showMessageDialog(null, "Erro na classe de conexão do BD" + erro.getMessage());
+//        }
+//        return conexao;
+//    }
+//
+//}
